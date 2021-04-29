c631fa3415ebf1a50a0fe3f1951a708faa824cf2
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2021-04-29 19:19:49 +0200

    "Arreglar Error LoginApiService"

--- src/app/_services/authguard.guard.ts
+++ src/app/_services/authguard.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { LoginService } from '../login/services/login.service';
-import { LoginApiService } from './login-api.service';
+
 
 @Injectable({
     providedIn: 'root'
