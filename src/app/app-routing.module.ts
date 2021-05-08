b27a90719cc844baf7ba2191fec4a5a95dfe45d5
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2021-05-08 18:11:47 +0200

    Cambio Estructura Rutas, Hasta que Sepamos como separarlo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthguardGuard } from './_services/authguard.guard';
+
+// Principales Componentes
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+
+// Profile Componentes
+import { ProfileComponent } from './home/components/pages/profile/profile.component';
+import { ProfileIndexComponent } from './home/components/pages/profile/profile-index/profile-index.component';
+import { ChangePassComponent } from './home/components/pages/profile/change-pass/change-pass.component';
+import { ChangePhotoComponent } from './home/components/pages/profile/change-photo/change-photo.component';
+
+// Empleado Componentes
+import { EmpleadosComponent } from './home/components/pages/empleados/empleados.component';
+import { EmpleadosIndexComponent } from './home/components/pages/empleados/empleados-index/empleados-index.component';
+import { AddEmpleadoComponent } from './home/components/pages/empleados/add-empleado/add-empleado.component';
+
 
 const routes: Routes = [
   { path: '', component:LoginComponent},
   { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard', component: HomeComponent, canActivate: [AuthguardGuard],
+    children: [
+       { path: '', component: DashboardComponent },
+      {
+        path: 'profile', component: ProfileComponent, children: [
+          { path: '', component: ProfileIndexComponent },
+          { path: 'cambiarClave', component: ChangePassComponent},
+          { path: 'cambiarFoto', component: ChangePhotoComponent}
+        ]
+      },
+      {
+        path: 'empleados', component: EmpleadosComponent, children: [
+          { path: '', component: EmpleadosIndexComponent },
+          { path: 'add-empleado', component: AddEmpleadoComponent},
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppRouterModule } from './_services/app-router/app-router.module';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
+
+// Principales Componentes
 import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './home/components/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+
+// Profile Componentes
+import { ProfileComponent } from './home/components/pages/profile/profile.component';
+import { ProfileIndexComponent } from './home/components/pages/profile/profile-index/profile-index.component';
 import { ChangePassComponent } from './home/components/pages/profile/change-pass/change-pass.component';
 import { ChangePhotoComponent } from './home/components/pages/profile/change-photo/change-photo.component';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
+
+// Empleado Componentes
+import { EmpleadosComponent } from './home/components/pages/empleados/empleados.component';
+import { EmpleadosIndexComponent } from './home/components/pages/empleados/empleados-index/empleados-index.component';
+import { AddEmpleadoComponent } from './home/components/pages/empleados/add-empleado/add-empleado.component';
+import { NavbarComponent } from './home/components/navbar/navbar.component';
 
 
 
@@ -23,10 +33,14 @@ import { DashboardComponent } from './home/dashboard/dashboard.component';
     HomeComponent,
     LoginComponent,
     NavbarComponent,
+    ProfileComponent,
+    ProfileIndexComponent,
     ChangePassComponent,
     ChangePhotoComponent,
     DashboardComponent,
-  
+    EmpleadosComponent,
+    EmpleadosIndexComponent,
+    AddEmpleadoComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +48,6 @@ import { DashboardComponent } from './home/dashboard/dashboard.component';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
