db2437864b42b431c2a8b12d530999e1e6a0e661
attiaApp <attiapp123@gmail.com>
2021-05-15 17:29:32 +0200

    newemployee.php

--- php/empleados/newemployee.php
+++ php/empleados/newemployee.php
@@ -7,10 +7,17 @@ if (isset($postdata) && !empty($postdata)) {
 
     $request = json_decode($postdata)->data;
 
-    $sql = "INSERT INTO employees (name,surname,dni,salary,phone_number,position) VALUES ('$request->name','$request->surname','$request->dni','$request->salary','$request->telf','$request->position');";
-    if ($result = mysqli_query($mysqli, $sql)) {
-        $exit = true;
-       
+    $sql2 = "SELECT * FROM employees WHERE dni='$request->dni'";
+    $result = mysqli_query($mysqli, $sql2);
+    $row = mysqli_num_rows($result);
+    if($row === 0){
+        $sql = "INSERT INTO employees (name,surname,dni,salary,phone_number,position) VALUES ('$request->name','$request->surname','$request->dni','$request->salary','$request->telf','$request->position');";
+        if ($result = mysqli_query($mysqli, $sql)) {
+            $exit = true;
+           
+        }
+    }else{
+        $exit = false;
     }
     echo json_encode($exit);
 }
