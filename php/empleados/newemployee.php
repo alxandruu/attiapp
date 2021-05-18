5ea020e1b13ba2e4724de96f4d754c3fb26ab3ba
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2021-05-18 19:05:08 +0200

    Comprobar que no exista un mismo usuario introducido (por DNI)

--- php/empleados/newemployee.php
+++ php/empleados/newemployee.php
@@ -6,15 +6,18 @@ if (isset($postdata) && !empty($postdata)) {
     $exit = false;
 
     $request = json_decode($postdata)->data;
+  
+    $name = strtoupper($request->name);
+    $surname = strtoupper($request->surname);
+    $dni = strtoupper($request->dni);
+    $position = strtoupper($request->position);
 
-    $sql2 = "SELECT * FROM employees WHERE dni='$request->dni'";
-    $result = mysqli_query($mysqli, $sql2);
-    $row = mysqli_num_rows($result);
+    $sql2 = "SELECT * FROM employees WHERE dni='$dni'";
+    $row = mysqli_num_rows(mysqli_query($mysqli, $sql2));  
     if($row === 0){
-        $sql = "INSERT INTO employees (name,surname,dni,salary,phone_number,position) VALUES ('$request->name','$request->surname','$request->dni','$request->salary','$request->telf','$request->position');";
+        $sql = "INSERT INTO employees (name,surname,dni,salary,phone_number,position) VALUES ('$name','$surname','$dni','$request->salary','$request->telf','$position');";
         if ($result = mysqli_query($mysqli, $sql)) {
             $exit = true;
-           
         }
     }else{
         $exit = false;
