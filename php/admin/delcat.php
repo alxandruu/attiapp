8702706fd78bfbf561f88de1a44a0c04fcb3ecd0
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2021-06-08 17:41:08 +0200

    FINAL!!!!

--- php/admin/delcat.php
+++ php/admin/delcat.php
@@ -3,16 +3,16 @@ include_once("../database.php");
 $postdata = file_get_contents("php://input");
 
 if (isset($postdata) && !empty($postdata)) {
-  
+
     $request = json_decode($postdata);
 
     $id = $request->id;
 
     $sql = "DELETE FROM categoria WHERE id='$id'";
-    if ($result = mysqli_query($mysqli, $sql) && mysqli_query($mysqli, "DELETE FROM ganancias WHERE id_cat='$id'") ) {
+    if ($result = mysqli_query($mysqli, $sql) && mysqli_query($mysqli, "DELETE FROM ganancias WHERE id_cat='$id'") && mysqli_query($mysqli, "DELETE FROM gastos WHERE id_cat='$id'")) {
         echo json_encode(true);
-    }else {
+    } else {
         http_response_code(404);
     }
-   
+    
 }
